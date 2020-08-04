684d750943434d5e8abc8cb1b63972c431607d00
Amaury Martin <amaury.conde@hotmail.com>
2020-08-04 19:59:17 -0300

    fixing album model specs

--- app/models/album.rb
+++ app/models/album.rb
@@ -3,4 +3,11 @@ class Album < ApplicationRecord
   has_many :players, through: :collabs
 
   validates_presence_of :name
+  validate :has_one_player_at_least
+
+  def has_one_player_at_least
+    if players.empty?
+      errors.add(:players, "need one player at least")
+    end
+  end
 end
--- test/models/album_test.rb
+++ test/models/album_test.rb
@@ -1,8 +1,15 @@
 require 'test_helper'
 
 class AlbumTest < ActiveSupport::TestCase
-  test "valid album" do
-    album = Album.new(name: 'Peligro', player: players(:shakira))
+  test "valid album with one player" do
+    players << players(:linkin_park)
+    album = Album.new(name: 'Minutes to Midnight', players: players)
+    assert album.valid?
+  end
+
+  test "valid album with two or more players" do
+    players << players(:jay_z, :linkin_park)
+    album = Album.new(name: 'Collision Course', players:players)
     assert album.valid?
   end
 
@@ -15,6 +22,6 @@ class AlbumTest < ActiveSupport::TestCase
   test "presence of player" do
     album = Album.new
     assert_not album.valid?
-    assert_not_empty album.errors[:player]
+    assert_not_empty album.errors[:players]
   end
 end
